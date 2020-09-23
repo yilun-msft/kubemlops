9b7c694c883535b878b33dbacdc1daf737a93525
Jovana Taylor <jotaylo@microsoft.com>
2020-09-22 17:09:58 -0700

    add external minio gateway

--- setup/kfp/kubeflow-install.sh
+++ setup/kfp/kubeflow-install.sh
@@ -5,7 +5,7 @@ kubectl apply -k github.com/kubeflow/pipelines/manifests/kustomize/cluster-scope
 kubectl wait crd/applications.app.k8s.io --for condition=established --timeout=60s
 
 
-# !! Comment out this section if using Azure MySQL !!
+### Comment out this section if using Azure MySQL or MinIO ###
 # Update Argo workflow config to use "pns" containerRuntimeExecutor instead of default "docker"
 # to support containerd
 # https://github.com/kubeflow/pipelines/issues/1654
@@ -13,9 +13,9 @@ kubectl wait crd/applications.app.k8s.io --for condition=established --timeout=6
 kubectl patch configmap/workflow-controller-configmap -n kubeflow --patch  "$(cat workflow-controller-configmap-patch.yaml)"
 kubectl apply -k github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic
 
-# !! Uncomment this section if using Azure MySQL !!
+### Uncomment this section if using Azure MySQL or MinIO ###
 # Remember to update the missing values in .env files first
-# kubectl apply -k ./mysql
+# kubectl apply -k ./azure
 
 # Apply this role and binding to fix the permission issue of cache-deployer-deployment
 # https://github.com/kubeflow/pipelines/pull/4246
